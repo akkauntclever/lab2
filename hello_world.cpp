3b07538e5a4427d5bc73b29188a69eca9cee0d2d
akkauntclever <fncps777@gmail.com>
2024-04-04 10:01:17 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 int main() {
-  std::cout << "Enter your username:" << std::endl;
+  std::cout << "Enter your username:" << std::endl;  //  Вывод информации о необходимости получить username
   std::string username;
-  std::getline(std::cin, username);
-  std::cout << "Hello, world from " << username << std::endl;
+  std::getline(std::cin, username);  //  получение username
+  std::cout << "Hello, world from " << username << std::endl;  //  вывод username
   return 0;
 }
 
